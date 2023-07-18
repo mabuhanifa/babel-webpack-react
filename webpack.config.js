ccf7c349f438f1883c96e760c50a6cb94ce3b879
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-18 16:15:57 +0600

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
 const path = require("path");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development",
+  entry: "./index.js",
   output: {
-    path: path.join(__dirname, "/build"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
   },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+
+  target: "web",
+  devServer: {
+    port: "3000",
+    static: ["./public"],
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts"],
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
+        use: "babel-loader",
       },
     ],
   },
